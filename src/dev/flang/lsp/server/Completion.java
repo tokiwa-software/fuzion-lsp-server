f7798152c62455115f722147eafe5e58c9a95b15
michael.lill <michael.lill@posteo.de>
2021-08-19 17:05:47 +0200

    add completion snippet for-in-loop

--- src/dev/flang/lsp/server/Completion.java
+++ src/dev/flang/lsp/server/Completion.java
@@ -6,20 +6,39 @@ import org.eclipse.lsp4j.CompletionItem;
 import org.eclipse.lsp4j.CompletionItemKind;
 import org.eclipse.lsp4j.CompletionList;
 import org.eclipse.lsp4j.CompletionParams;
+import org.eclipse.lsp4j.InsertTextFormat;
+import org.eclipse.lsp4j.InsertTextMode;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 
 public class Completion {
 
-  private static CompletionItem createTextCompletionItem(String label, Object data) {
+  private static CompletionItem buildSnippet(String label, String insertText) {
     var item = new CompletionItem(label);
-    item.setKind(CompletionItemKind.Text);
-    item.setData(data);
+    item.setKind(CompletionItemKind.Snippet);
+    item.setInsertTextFormat(InsertTextFormat.Snippet);
+    item.setInsertTextMode(InsertTextMode.AdjustIndentation);
+    item.setInsertText(insertText);
     return item;
   }
 
-  public static Either<List<CompletionItem>, CompletionList> getCompletions(CompletionParams position) {
-    List<CompletionItem> completionItems = Arrays.asList(createTextCompletionItem("test1", 1));
-    return Either.forLeft(completionItems);
+  public static Either<List<CompletionItem>, CompletionList> getCompletions(CompletionParams params) {
+    var word = getWord(params);
+    switch (word) {
+    case "for":
+      return Either.forLeft(Arrays.asList(buildSnippet("for i in start..end do", "for ${1:i} in ${2:0}..${3:10} do")));
+    }
+    return Either.forLeft(List.of());
+  }
+
+  private static String getWord(CompletionParams params) {
+    var text = FuzionTextDocumentService.getText(params.getTextDocument().getUri());
+    var line = text.split("\n")[params.getPosition().getLine()];
+    var start = params.getPosition().getCharacter();
+    do {
+      --start;
+    } while (start >= 0 && line.charAt(start) != ' ');
+    var word = line.substring(start + 1, params.getPosition().getCharacter());
+    return word;
   }
 
 }
