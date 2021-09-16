386a4901b1bd7fa7d83e8645280e0f3c1c23cdea
Michael Lill <michael.lill@tokiwa.software>
2021-09-16 13:00:01 +0200

    Launcher set custom exception handler to exit on every error

--- src/dev/flang/lsp/server/Main.java
+++ src/dev/flang/lsp/server/Main.java
@@ -1,68 +1,89 @@
 package dev.flang.lsp.server;
+
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.services.LanguageClient;
 
-public class Main {
+public class Main
+{
 
-  enum Transport {
+  enum Transport
+  {
     stdio, tcp
   }
 
   private static LanguageClient _languageClient;
   public static Transport transport;
 
-  public static LanguageClient getLanguageClient(){
+  public static LanguageClient getLanguageClient()
+  {
     return _languageClient;
   }
 
-  public static boolean DEBUG(){
+  public static boolean DEBUG()
+  {
     var debug = System.getenv("DEBUG");
-    if(debug == null){
-      return false;
-    }
+    if (debug == null)
+      {
+        return false;
+      }
     return debug.toLowerCase().equals("true");
   };
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception
+  {
 
     System.setProperty("FUZION_DISABLE_ANSI_ESCAPES", "true");
 
-    transport = Arrays.stream(args).map(arg -> arg.trim().toLowerCase()).anyMatch("-tcp"::equals) ? Transport.tcp
-        : Transport.stdio;
-
-    switch (transport) {
-    case stdio:
-      _languageClient= startStdIo();
-      break;
-    case tcp:
-      _languageClient= startTcpServer();
-      break;
-    }
-  }
-
-  private static LanguageClient startTcpServer() throws IOException, InterruptedException, ExecutionException {
-    try (var serverSocket = new ServerSocket(0)) {
-
-      System.out.println("socket opened on port: " + serverSocket.getLocalPort());
-      var socket = serverSocket.accept();
-
-      var server = new FuzionLanguageServer();
-      var launcher = Launcher.createLauncher(server, LanguageClient.class, socket.getInputStream(),
-          socket.getOutputStream());
-      launcher.startListening();
-      return launcher.getRemoteProxy();
-    }
-  }
-
-  private static LanguageClient startStdIo() throws InterruptedException, ExecutionException {
-    var server = new FuzionLanguageServer();
-    var launcher = Launcher.createLauncher(server, LanguageClient.class, System.in, System.out);
+    transport = Arrays.stream(args).map(arg -> arg.trim().toLowerCase()).anyMatch("-tcp"::equals)
+                                                                                                  ? Transport.tcp
+                                                                                                  : Transport.stdio;
+    var launcher = getLauncher();
     launcher.startListening();
-    return launcher.getRemoteProxy();
+    _languageClient = launcher.getRemoteProxy();
   }
+
+  private static Launcher<LanguageClient> getLauncher() throws InterruptedException, ExecutionException, IOException
+  {
+    var server = new FuzionLanguageServer();
+    switch (transport)
+      {
+      case stdio :
+        return createLauncher(server, System.in, System.out);
+      case tcp :
+        try (var serverSocket = new ServerSocket(0))
+          {
+
+            System.out.println("socket opened on port: " + serverSocket.getLocalPort());
+            var socket = serverSocket.accept();
+            return createLauncher(server, socket.getInputStream(), socket.getOutputStream());
+          }
+      default:
+        System.exit(1);
+        return null;
+      }
+  }
+
+  private static Launcher<LanguageClient> createLauncher(FuzionLanguageServer server, InputStream in, OutputStream out)
+    throws IOException
+  {
+    return new Launcher.Builder<LanguageClient>()
+      .setLocalService(server)
+      .setRemoteInterface(LanguageClient.class)
+      .setInput(in)
+      .setOutput(out)
+      .setExceptionHandler((x) -> {
+        System.exit(1);
+        return null;
+      })
+      .create();
+  }
+
+
 }
