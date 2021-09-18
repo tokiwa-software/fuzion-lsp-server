3777a07b58ecaa70005bd19ece1b98fc47f49b0d
Michael Lill <michael.lill@tokiwa.software>
2021-09-18 12:43:47 +0200

    getEndOfFeature refactoring to use HeirsVisitor

--- src/dev/flang/lsp/server/FuzionHelpers.java
+++ src/dev/flang/lsp/server/FuzionHelpers.java
@@ -260,150 +260,21 @@ public class FuzionHelpers
   {
     if (!Memory.EndOfFeature.containsKey(baseFeature))
       {
-        var positions = new TreeSet<SourcePosition>();
-        baseFeature.visit(new FeatureVisitor() {
-          @Override
-          public Stmnt action(Feature f, Feature outer)
-          {
-            positions.add(getPosition(f));
-            f.visit(this);
-            f.declaredFeatures().forEach((n, df) -> df.visit(this, f));
-            return super.action(f, outer);
-          }
+        SourcePosition endOfFeature = HeirsVisitor
+          .visit(baseFeature)
+          .entrySet()
+          .stream()
+          .filter(entry -> entry.getValue() != null)
+          .filter(entry -> entry.getValue().compareTo(baseFeature) == 0)
+          .map(entry -> getPosition(entry.getKey()))
+          .sorted((Comparator<SourcePosition>) Comparator.<SourcePosition>reverseOrder())
+          .map(position -> {
+            return new SourcePosition(position._sourceFile, position._line, getEndColumn(position));
+          })
+          .findFirst()
+          .orElse(baseFeature.pos());
 
-          @Override
-          public void action(Unbox u, Feature outer)
-          {
-            positions.add(getPosition(u));
-          }
-
-          @Override
-          public void action(Assign a, Feature outer)
-          {
-            positions.add(getPosition(a));
-          }
-
-          @Override
-          public void actionBefore(Block b, Feature outer)
-          {
-            positions.add(getPosition(b));
-          }
-
-          @Override
-          public void actionAfter(Block b, Feature outer)
-          {
-            positions.add(getPosition(b));
-          }
-
-          @Override
-          public void action(Box b, Feature outer)
-          {
-
-            positions.add(getPosition(b));
-          }
-
-          @Override
-          public Expr action(Call c, Feature outer)
-          {
-            positions.add(getPosition(c));
-            return c;
-          }
-
-          @Override
-          public void actionBefore(Case c, Feature outer)
-          {
-            positions.add(getPosition(c));
-          }
-
-          @Override
-          public void actionAfter(Case c, Feature outer)
-          {
-            positions.add(getPosition(c));
-          }
-
-          @Override
-          public void action(Cond c, Feature outer)
-          {
-            // Cond has no SourcePosition, not including
-          }
-
-          @Override
-          public Expr action(Current c, Feature outer)
-          {
-            positions.add(getPosition(c));
-            return c;
-          }
-
-          @Override
-          public Stmnt action(Destructure d, Feature outer)
-          {
-            positions.add(getPosition(d));
-            return d;
-          }
-
-          @Override
-          public Expr action(Function f, Feature outer)
-          {
-            positions.add(getPosition(f));
-            return f;
-          }
-
-          @Override
-          public void action(Generic g, Feature outer)
-          {
-            positions.add(getPosition(g));
-          }
-
-          @Override
-          public void action(If i, Feature outer)
-          {
-            positions.add(getPosition(i));
-          }
-
-          @Override
-          public void action(Impl i, Feature outer)
-          {
-            positions.add(getPosition(i));
-          }
-
-          @Override
-          public Expr action(InitArray i, Feature outer)
-          {
-            positions.add(getPosition(i));
-            return i;
-          }
-
-          @Override
-          public void action(Match m, Feature outer)
-          {
-            positions.add(getPosition(m));
-          }
-
-          @Override
-          public void action(Tag b, Feature outer)
-          {
-            positions.add(getPosition(b));
-          }
-
-          @Override
-          public Expr action(This t, Feature outer)
-          {
-            positions.add(getPosition(t));
-            return t;
-          }
-
-          @Override
-          public Type action(Type t, Feature outer)
-          {
-            positions.add(getPosition(t));
-            return t;
-          }
-        }, baseFeature.outer());
-
-        var endColumn = getEndColumn(positions.last());
-
-        Memory.EndOfFeature.put(baseFeature,
-          new SourcePosition(positions.last()._sourceFile, positions.last()._line, endColumn));
+        Memory.EndOfFeature.put(baseFeature, endOfFeature);
       }
 
     return Memory.EndOfFeature.get(baseFeature);
