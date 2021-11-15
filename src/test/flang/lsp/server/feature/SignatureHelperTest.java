ac9200e2bb00762c999ec938b783dca5b28686c2
Michael Lill <michael.lill@tokiwa.software>
2021-11-15 16:13:41 +0100

    formatting

--- src/test/flang/lsp/server/feature/SignatureHelperTest.java
+++ src/test/flang/lsp/server/feature/SignatureHelperTest.java
@@ -45,28 +45,28 @@ public class SignatureHelperTest extends BaseTest
     """;
 
   private static final String Mandelbrot = """
-  mandelbrotexample is
-    isInMandelbrotSet(c complex<f64>, maxEscapeIterations i32, z complex<f64>) bool is
-      maxEscapeIterations = 0 || z.abs² <= 4 && isInMandelbrotSet c maxEscapeIterations-1 z*z+c
+    mandelbrotexample is
+      isInMandelbrotSet(c complex<f64>, maxEscapeIterations i32, z complex<f64>) bool is
+        maxEscapeIterations = 0 || z.abs² <= 4 && isInMandelbrotSet c maxEscapeIterations-1 z*z+c
 
-    # NYI how to convert i32 to f64?
-    to_f64(i i32) f64 is
-      if i > 0 1.0 + to_f64(i - 1) else 1.0
+      # NYI how to convert i32 to f64?
+      to_f64(i i32) f64 is
+        if i > 0 1.0 + to_f64(i - 1) else 1.0
 
-    steps(start, step f64, numPixels i32) =>
-      array<f64> numPixels (i -> start + to_f64(i) * step)
+      steps(start, step f64, numPixels i32) =>
+        array<f64> numPixels (i -> start + to_f64(i) * step)
 
-    mandelbrotImage(yStart, yStep, xStart, xStep f64, height, width i32) =>
-      for y in steps yStart yStep height do
-        for x in steps xStart xStep width do
-          if isInMandelbrotSet (complex x y) 50 (complex 0.0 0.0)
-            yak "⬤"
-          else
-            yak
-        say ""
+      mandelbrotImage(yStart, yStep, xStart, xStep f64, height, width i32) =>
+        for y in steps yStart yStep height do
+          for x in steps xStart xStep width do
+            if isInMandelbrotSet (complex x y) 50 (complex 0.0 0.0)
+              yak "⬤"
+            else
+              yak
+          say ""
 
-    mandelbrotImage 1 -0.05 -2 0.0315 40 80
-  """;
+      mandelbrotImage 1 -0.05 -2 0.0315 40 80
+    """;
 
   @Test
   public void getSignatureHelpMultipleSignatures()
@@ -81,13 +81,16 @@ public class SignatureHelperTest extends BaseTest
   {
     SourceText.setText(uri1, Mandelbrot);
     assertEquals("yak(s Object) => unit", LabelAt(uri1, new Position(17, 13), 0));
-    assertEquals("mandelbrotImage(yStart f64, yStep f64, xStart f64, xStep f64, height i32, width i32) => unit", LabelAt(uri1, new Position(20, 17), 0));
+    assertEquals("mandelbrotImage(yStart f64, yStep f64, xStart f64, xStep f64, height i32, width i32) => unit",
+      LabelAt(uri1, new Position(20, 17), 0));
   }
 
   private String LabelAt(URI uri, final Position position, int index)
   {
     return SignatureHelper.getSignatureHelp(new SignatureHelpParams(LSP4jUtils.TextDocumentIdentifier(uri), position))
-    .getSignatures().get(index).getLabel();
+      .getSignatures()
+      .get(index)
+      .getLabel();
   }
 
 }
