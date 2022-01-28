54413ef04efebc05df0f26d52bf0e5e4b7892b14
Michael Lill <michael.lill@tokiwa.software>
2022-01-28 16:00:29 +0100

    improve tests for "unused" diagnostics

--- src/test/flang/lsp/server/feature/DiagnosticsTest.java
+++ src/test/flang/lsp/server/feature/DiagnosticsTest.java
@@ -26,6 +26,8 @@ Fuzion language implementation.  If not, see <https://www.gnu.org/licenses/>.
 
 package test.flang.lsp.server.feature;
 
+import java.util.stream.Collectors;
+
 import org.eclipse.lsp4j.DiagnosticTag;
 import org.junit.jupiter.api.Test;
 
@@ -58,8 +60,27 @@ public class DiagnosticsTest extends ExtendedBaseTest
               say b
             """;
         SourceText.setText(uri1, sourceText);
-        var diagnostics = Diagnostics.getDiagnostics(uri1);
-        assertTrue(diagnostics.findFirst().get().getTags().get(0).equals(DiagnosticTag.Unnecessary));
+        var diagnostics = Diagnostics.getDiagnostics(uri1).collect(Collectors.toList());
+        assertEquals(1, diagnostics.size());
+        assertTrue(diagnostics.get(0).getTags().get(0).equals(DiagnosticTag.Unnecessary));
+    }
+
+    // @Test
+    // NYI failing
+    public void UnusedArgument()
+    {
+        var sourceText = """
+            storage =>
+              store(data array<store>|array<nil>) is
+              say "asdf"
+                        """;
+        SourceText.setText(uri1, sourceText);
+        var diagnostics = Diagnostics.getDiagnostics(uri1).collect(Collectors.toList());
+        assertEquals(2, diagnostics.size());
+        assertEquals(1, diagnostics.get(0).getRange().getStart().getLine());
+        assertEquals(2, diagnostics.get(0).getRange().getStart().getCharacter());
+        assertEquals(1, diagnostics.get(0).getRange().getEnd().getLine());
+        assertEquals(37, diagnostics.get(0).getRange().getEnd().getCharacter());
     }
 
 }
